31934c4305f542b3279d135e92c80e2881e281b5
mausch <mauricioscheffer@gmail.com>
2016-06-03 13:39:01 +0300

    minor

--- AdvancedCQRS.DocumentMessaging/AdvancedCQRS.DocumentMessaging/Monitor.cs
+++ AdvancedCQRS.DocumentMessaging/AdvancedCQRS.DocumentMessaging/Monitor.cs
@@ -10,7 +10,7 @@ namespace AdvancedCQRS.DocumentMessaging
     {
         public void Handle(MessageBase @event)
         {
-            Console.WriteLine(@event);
+            Console.WriteLine($"{@event} id {@event.Id} correlation {@event.CorrelationId} cause {@event.CauseId}");
         }
     }
 }
